2f5b7b08268dbdb16d19ff6df6209d95778209e6
Andres Diaz <andres.diaz@getbuilt.com>
2020-12-15 21:45:56 -0500

    Initial Commit

---
+++ MetalRenderer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalRenderer
+//
+//  Created by Andres Diaz on 12/15/20.
+//  Copyright © 2020 Andres Diaz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalRenderer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalRenderer
+//
+//  Created by Andres Diaz on 12/15/20.
+//  Copyright © 2020 Andres Diaz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
